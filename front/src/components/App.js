42983c92d4d2e9d7c1494a63fcd830ee4d700a12
Loan Albiach <loanalbiach@hotmail.fr>
2024-01-04 16:48:39 +0100

    Partage du front

---
+++ front/src/components/App.js
@@ -0,0 +1,12 @@
+import '../style/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
+
+export default App;
