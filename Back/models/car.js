83ab4ae6a96e4ce9e8038ccf37b1885e1dd9fd66
CarTig <carolle.t.n@gmail.com>
2024-01-04 17:29:26 +0100

    Model Car completé

---
+++ Back/models/car.js
@@ -0,0 +1,38 @@
+const sequelize = require('../database/database')
+const {DataTypes} = require('sequelize')
+//ici importation des modeles avec lesquels lier cette table
+
+
+//table Car et champs
+const Car = sequelize.define('car', {
+    //première colonne
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoincrement: true
+    },
+    //deuxième colonne
+    name: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    //troisième colonne
+    door: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    //quatrième colonne
+    engine: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+     //cinqième colonne
+     seating_capacity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+});
+
+module.exports = Car
