6a3d968361049c87c05879a2972b46c06cb5137e
William Loyer <w.loyer@outlook.com>
2024-01-05 14:45:36 +0100

    serv + model

--- Back/server.js
+++ Back/server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const cors = require ('cors')
 
 const databaseRoute = require('./routes/databaseRoute')
+const userRoute = require('./routes/userRoute')
+const carRoute = require('./routes/carRoute')
+const optionRoute = require('./routes/optionRoute')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
 app.use('/database', databaseRoute)
+app.use('/user', userRoute)
+app.use('/car', carRoute)
+app.use('/option', optionRoute)
 
 
 app.listen(8000, ()=>{
