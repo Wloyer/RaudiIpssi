deefba10d21ca4b4573f5bd477fd4dbf6e214a60
CarTig <carolle.t.n@gmail.com>
2024-01-05 11:55:27 +0100

    controller & route de Car get & delete

--- Back/controllers/carControllers.js
+++ Back/controllers/carControllers.js
@@ -1,5 +1,13 @@
 const Car = require('../models/carModel')
 
+exports.getAllCar = async(req,res) => {
+    let allCar = await Car.findAll();
+    res.status(200).json({
+        message:"voici toutes les voitures",
+        car:allCar
+    })
+};
+
 exports.CreateCar = async(req,res)=>{
     let car = req.body 
     await Car.create(car)
@@ -7,13 +15,32 @@ exports.CreateCar = async(req,res)=>{
 };
 
 exports.UpdateCar = async(req,res)=>{
-    let id = req.params.id
-    let UpdateCar = req.body
+    let idC = req.params.id
+    let nameC = req.params.name
+    let doorC = req.params.door
+    let engineC = req.params.engine
+    let seating_capacityC = req.params.seating_capacity
+    let UpdateDataCar = req.body
 
-    let car = await Car.update({UpdateCar},{
+    let car = await Car.update(UpdateDataCar,{
+        where: {
+            id: idC,
+            name: nameC,
+            door: doorC,
+            engine: engineC,
+            seating_capacity: seating_capacityC
+        }
+    })
+    res.status(200).json(car)
+};
+
+exports.DeleteCar = async(req,res) => {
+    let idC = req.params.id
+    
+    let deleteCar = await Car.destroy({
         where: {
             id: idC
         }
     })
-    res.status(200).json(car)
+    res.status(200).json(deleteCar)
 }
