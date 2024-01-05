5baded7f360e9ea63790745d2c4cd494846226ad
Loan Albiach <loanalbiach@hotmail.fr>
2024-01-05 10:54:29 +0100

    Mise en place du .env pour la BDD

--- Back/database/database.js
+++ Back/database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('Raudi', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_DTB, process.env.DB_USER, process.env.DB_PWD, {
+    host: process.env.DB_HOST,
     dialect: 'mariadb'
 })
 
