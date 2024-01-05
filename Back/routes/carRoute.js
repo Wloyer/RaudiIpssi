6cd9cd0130d84ac9f23b94890cdcee4c70168876
Loan Albiach <loanalbiach@hotmail.fr>
2024-01-05 16:31:29 +0100

    mmh

--- Back/routes/carRoute.js
+++ Back/routes/carRoute.js
@@ -4,11 +4,7 @@ const carController = require('../controllers/carControllers')
 
 route.get('/getAllCar', carController.getAllCar)
 route.post('/createCar', carController.createCar)
-route.put('/updateCar', carController.updateCar)
-<<<<<<< HEAD
+route.put('/updateCar/:id', carController.updateCar)
 route.delete('/deleteCar/:id', carController.deleteCar)
-=======
-route.delete('/deleteCar', carController.deleteCar)
->>>>>>> f2761bf7c06b6b9a281ac3a4bc728db0f5bf8eb1
 
 module.exports = route;
