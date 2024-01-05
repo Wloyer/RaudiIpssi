b0c3d1bb8ae36322c21880f7ddb75290fd727903
Loan Albiach <loanalbiach@hotmail.fr>
2024-01-05 14:53:50 +0100

    Front style connexion et inscription

--- front/src/components/Inscription.js
+++ front/src/components/Inscription.js
@@ -1,13 +1,34 @@
 // Inscription.js
 import React from 'react';
-import '../style/Inscription.css';  
+import '../style/Inscription.css';
 
 function Inscription() {
   return (
-    <div>
-      <h2>Inscription</h2>
+    <div className="inscription-container">
+      <h2 className='h2Insc'>Inscription</h2>
+      <form className="inscription-form">
+        <label className='labelInsc' htmlFor="firstName">Prénom:</label>
+        <input className='inputInsc' type="text" id="firstName" name="firstName" required />
+
+        <label className='labelInsc' htmlFor="lastName">Nom:</label>
+        <input className='inputInsc' type="text" id="lastName" name="lastName" required />
+
+        <label className='labelInsc' htmlFor="email">Email:</label>
+        <input className='inputInsc' type="email" id="email" name="email" required />
+
+        <label className='labelInsc' htmlFor="password">Mot de passe:</label>
+        <input className='inputInsc' type="password" id="password" name="password" required />
+
+        <div className="remember-me">
+          <input className='inputInsc' type="checkbox" id="conditions" name="conditions" />
+          <label className='labelInsc' htmlFor="conditions">Conditions d'utilisations</label>
+          <a className='goToConnexion' href="/connexion">Je possède déjà un compte ?</a>
+        </div>
+
+        <button className='buttonInsc' type="submit">S'inscrire</button>
+      </form>
     </div>
   );
-};
+}
 
 export default Inscription;
