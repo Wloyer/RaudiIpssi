5e0c01466704c14d2fb074964f06b7bfb5b85082
William Loyer <w.loyer@outlook.com>
2024-01-08 17:39:35 +0100

    sans auth

--- front/src/components/Historique.js
+++ front/src/components/Historique.js
@@ -1,40 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import '../style/Accueil.css'; 
-import Cookies from 'js-cookie';
-
 
 function OrderHistory() {
   const [orders, setOrders] = useState([]);
-  const navigate = useNavigate();
-  const userRole = Cookies.get('role'); 
 
   useEffect(() => {
-    if (userRole !== 'admin' && userRole !== 'comptable') {
-      // Si l'utilisateur n'est ni admin ni comptable, redirigez vers la page d'accueil.
-      navigate('/');
-      return;
-    }
-
-    // Déterminez l'URL en fonction du rôle de l'utilisateur
-    const url = userRole === 'admin' || userRole === 'comptable' 
-      ? '/ordered/monthly' 
-      : '/ordered/history';
-
-    axios.get(url, {
-      headers: {
-        'Authorization': Cookies.get('token')
-      }
-    })
+    // La requête est effectuée sans vérification du rôle de l'utilisateur
+    axios.get('/ordered/history') // Utilisez l'URL appropriée pour votre API
     .then(response => {
       setOrders(response.data);
     })
     .catch(error => {
       console.error('Erreur lors de la récupération des données:', error);
-      navigate('/');
+      // Vous pouvez choisir de gérer l'erreur différemment, par exemple, afficher un message d'erreur
     });
-  }, [navigate, userRole]);
+  }, []);
 
   return (
     <div className="order-history-container">
@@ -53,9 +34,9 @@ function OrderHistory() {
           {orders.map(order => (
             <tr key={order.id}>
               <td>{order.id}</td>
-              <td>{order.price}</td>
               <td>{order.userId}</td> {/* Assurez-vous que ces champs correspondent à votre structure de données */}
               <td>{order.carId}</td>
+              <td>{order.price}</td>
             </tr>
           ))}
         </tbody>
